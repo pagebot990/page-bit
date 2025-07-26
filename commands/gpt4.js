bf27dd74b02ec592c2132071d28b416cfe895324
pagebot990 <pagebot990@gmail.com>
2025-07-26 11:23:13 +0800

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -2,32 +2,31 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'gpt4',
-  description: 'Interact with GPT-4o',
-  usage: 'gpt4 [your message]',
-  author: 'GeoDevz69',
-  
+    name: 'gpt4',
+    description: 'Interact with GPT-4o',
+    usage: 'gpt4 [your message]',
+    author: 'coffee',
 
-  async execute(senderId, args, pageAccessToken) {
-    const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+    async execute(senderId, args, pageAccessToken) {
+        const prompt = args.join(' ');
+        if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
-    try {
-      const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?ask=${encodeURIComponent(prompt)}&uid=${senderId}&webSearch=off&apikey=72f8161d-50d4-4177-a3b4-bd6891de70ef`);
+        try {
+            const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/kaiz-ai?ask=${encodeURIComponent(prompt)}&uid=${senderId}&apikey=8c0a049d-29a8-474a-b15e-189e42e150fb`);
 
-      const parts = [];
+            const parts = [];
 
-      for (let i = 0; i < response.length; i += 1999) {
-        parts.push(response.substring(i, i + 1999));
-      }
+            for (let i = 0; i < response.length; i += 1999) {
+                parts.push(response.substring(i, i + 1999));
+            }
 
-      // send all msg parts
-      for (const part of parts) {
-        await sendMessage(senderId, { text: part }, pageAccessToken, ' Sending...');
-      }
+            // send all msg parts
+            for (const part of parts) {
+                await sendMessage(senderId, { text: part }, pageAccessToken);
+            }
 
-    } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+        } catch {
+            sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+        }
     }
-  }
 };
